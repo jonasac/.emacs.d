d0a27d1190d2f60cb076f1ca09318c052e3ea6c5
jonasac <jonasac@student.matnat.uio.no>
2013-05-21 00:40:31 +0200

    spring cleaning

--- config/hooks.el
+++
@@ -1,6 +0,0 @@
-(add-hook 'minibuffer-exit-hook
-	  '(lambda ()
-	     (let ((buffer "*Completions*"))
-	       (and (get-buffer buffer)
-		    (kill-buffer buffer)))))
-(provide 'hooks)
--- config/osx.el
+++
@@ -1,5 +0,0 @@
-;; Fix metakey
-(exec-path-from-shell-initialize)
-(setq mac-option-modifier nil
-      mac-command-modifier 'meta)
-(provide 'osx)
