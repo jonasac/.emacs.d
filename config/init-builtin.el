27838ef70445f88585fa62f74e10fded40623821
jonasac <jonasac@student.matnat.uio.no>
2013-07-02 11:42:28 +0200

    remove fringe setting

--- config/init-builtin.el
+++ config/init-builtin.el
@@ -6,7 +6,6 @@
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(set-fringe-mode '(8 . 0))
 ;;;; OSX specific settings
 (when (eq system-type 'darwin)
   (setq mac-option-modifier nil
