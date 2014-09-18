61cad255ed6ff0a6a538df93494cfeef4fc9878f
Jonas Arnold Clasen <j.a.clasen@usit.uio.no>
2014-09-18 11:16:53 +0200

    Add font settings for linux systems

--- config/system-specific.el
+++ config/system-specific.el
@@ -1,9 +1,13 @@
 ;;;; OSX
-(when (eq system-type 'darwin)
-  (require 'exec-path-from-shell)
-  (exec-path-from-shell-initialize)
+(when window-system
+  (when (eq system-type 'darwin)
+    (require 'exec-path-from-shell)
+    (exec-path-from-shell-initialize)
   (setq mac-option-modifier nil
 	mac-command-modifier 'meta)
   (set-face-attribute 'default nil :font "Inconsolata 13"))
+
+  (when (eq system-type 'gnu/linux)
+  (set-face-attribute 'default nil :font "Inconsolata 10")))
 
 (provide 'system-specific)
