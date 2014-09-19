59c5f8550aa8a9ce516b2118fc65156f6944b068
Jonas Arnold Clasen <j.a.clasen@usit.uio.no>
2014-09-19 21:47:23 +0200

    Move keybindings into init.el

--- init.el
+++ init.el
@@ -72,5 +72,37 @@
 (when (eq system-type 'gnu/linux)
   (set-face-attribute 'default nil :font "Inconsolata 10")))
 
+;;;; Keybindings
+
+;; Make it harder to quit
+(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+
+;; Start eshell or switch to it if it is active
+(global-set-key (kbd "C-x m") 'eshell)
+
+;; Jump to definitition in the current file
+(global-set-key (kbd "C-x C-i") 'imenu)
+
+;; Go M-x without pressing M
+(global-set-key (kbd "C-x m") 'execute-extended-command)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(global-set-key (kbd "C-x t") 'jac-toggle-eshell-visor)
+
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+(require 'key-chord)
+(key-chord-define-global "uu" 'undo-tree-visualize)
+(key-chord-define-global "jj" 'ace-jump-word-mode)
+(key-chord-define-global "jl" 'ace-jump-line-mode)
+(key-chord-define-global "jk" 'ace-jump-char-mode)
+(key-chord-mode +1)
 
 (load-theme 'solarized-dark t)
